66cb547a5eaa8fbfe765420fdc5d6d87b57e565a
tushar199627 <tusharbarai6@gmail.com>
2022-07-06 13:15:50 +0530

    updated

--- src/controller/usercontroller.js
+++ src/controller/usercontroller.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const usermodel = require("../model/usermode");
+const usermodel = require("../model/usermodel");
 
 
 //login user
@@ -11,11 +11,11 @@ let userlogin = async function (req, res) {
         // validation
 
         if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-            return res.status(400).send({ status: false, message: `Email should be a valid email address` });
+            return res.status(400).send({ status: false, message: `Email Should be a Valid Email Address` });
         }
 
         if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password)) {
-            return res.status(400).send({ status: false, message: `password should contain atleastone number or one alphabet and should be 8 character long` });
+            return res.status(400).send({ status: false, message: `Password should contain atleast one number or one alphabet and should be 8 character long` });
         }
 
         //validation end
@@ -31,20 +31,18 @@ let userlogin = async function (req, res) {
             batch: "radon",
             organisation: "FunctionUp",
             iat: new Date().getTime()
-        }, "stack", { expiresIn: "10h" },
-            "functionup-radon-group52"
-        )
+        },
+            "functionup-radon-group52",
+            {
+                expiresIn: "10h"
+            });
 
         res.setHeader("x-api-key", token);
 
-        return res.status(200).send({ status: true, token: token, msg: "user logged in successfully" });
+        return res.status(200).send({ status: true, token: token, msg: "User Logged in Successfully" });
 
 
-    }
-
-
-
-    catch (error) {
+    }catch (error) {
         res.status(500).send({ status: false, error: error.message })
 
     }
