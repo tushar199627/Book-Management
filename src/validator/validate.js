2aa121e34c646b85548c9ea2e5d78a3704282e6f
tushar199627 <tusharbarai6@gmail.com>
2022-07-06 12:26:26 +0530

    updated

---
+++ src/validator/validate.js
@@ -0,0 +1,13 @@
+let isValid= function(value){
+    if(typeof value === "undefined" || value === "null") return false;
+    if(typeof value === "string" && value.trim().length === 0) return false;
+    return true;
+};
+
+let isValidRequestBody = function(requestBody){
+    return Object.keys(requestBody).length != 0;
+};
+
+
+
+module.export= {isValid, isValidRequestBody}
