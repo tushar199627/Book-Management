12108131f1d6cd30d2fc9404ff2ac40780e96b7c
tushar199627 <tusharbarai6@gmail.com>
2022-07-08 12:19:45 +0530

    updated

--- src/model/reviewmodel.js
+++ src/model/reviewmodel.js
@@ -1,47 +1,46 @@
-const mongoose = require('mongoose')
-const objectId = mongoose.Schema.Types.ObjectId
+const mongoose = require("mongoose");
+const objectId = mongoose.Schema.Types.ObjectId;
 
+const userSchema = new mongoose.Schema(
+  {
+    bookId: {
+      type: objectId,
+      required: true,
+      ref: "bookDb",
+      trim: true,
+    },
 
-const userSchema = new mongoose.Schema ({
+    reviewedBy: {
+      type: String,
+      required: true,
+      default: "Guest",
+      trim: true,
+    },
 
- bookId: {
-    type: objectId,
-    required: true,
-    ref: 'bookDb',
-    trim: true
- },
+    reviewedAt: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
 
- reviewedBy: {
-  type: String,
-  required: true,
-  default: "Guest",
-  trim: true,
-},
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+      trim: true,
+    },
 
-reviewedAt: {
-  type: Date,
-  required: true,
-  trim: true,
-},
+    review: {
+      type: String,
+      trim: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-rating: {
-  type: Number,
-  min: 1,
-  max: 5,
-  required: true,
-  trim: true,
-},
-
-review: {
-  type: String,
-  trim: true,
-},
-isDeleted: {
-  type: Boolean,
-  default: false,
-}
-
-}, { timestamps: true });
-
-
-module.exports = mongoose.model ('review', userSchema)
+module.exports = mongoose.model("review", userSchema);
